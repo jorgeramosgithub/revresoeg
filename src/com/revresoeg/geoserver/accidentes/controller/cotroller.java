c57b0076dfb2566bd5cbeaf9962457c89def6f7f
Magda <magdamcmb@yahoo.com.mx>
2016-12-11 17:33:41 -0600

    g

---
+++ src/com/revresoeg/geoserver/accidentes/controller/cotroller.java
@@ -0,0 +1,7 @@
+package com.revresoeg.geoserver.accidentes.controller;
+
+/**
+ * Created by Magda on 11/12/2016.
+ */
+public class cotroller {
+}
---
+++ src/com/revresoeg/geoserver/accidentes/model/model.java
@@ -0,0 +1,7 @@
+package com.revresoeg.geoserver.accidentes.model;
+
+/**
+ * Created by Magda on 11/12/2016.
+ */
+public class model {
+}
