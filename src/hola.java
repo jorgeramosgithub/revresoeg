c0cabd9f1948d244f310b9b981eb820208f547a5
Jorge Ramos <jramos@consistent.mx>
2016-12-11 15:36:53 -0600

    TEst

---
+++ src/hola.java
@@ -0,0 +1,12 @@
+import example.HelloWorld;
+
+import javax.xml.ws.Endpoint;
+
+/**
+ * Created by Jorge on 11/12/2016.
+ */
+public class hola {
+    public static void main(String[] argv) {
+        System.out.print("Hola Edmundo!");
+    }
+}
