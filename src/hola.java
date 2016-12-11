1bde1a1ddc671d3d46cdd88e14cc477d396c9274
Magda <magdamcmb@yahoo.com.mx>
2016-12-11 15:59:27 -0600

    g

--- src/hola.java
+++ src/hola.java
@@ -7,6 +7,6 @@ import javax.xml.ws.Endpoint;
  */
 public class hola {
     public static void main(String[] argv) {
-        System.out.print("Hola magda!");
+        System.out.print("Hola magda");
     }
 }
